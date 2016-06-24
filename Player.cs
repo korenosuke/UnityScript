bc12b48198b158bec50d3861bab4bb217b814afa
RyosukeShinohara <15010411@anabuki-college.ac.jp>
2016-06-24 11:28:36 +0900

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -1,48 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public class Player:MonoBehaviour {
+	
+	
+	
+	public GameObject playerObject = null;		//モデルを指定
+	public GameObject bulletObject = null;		//弾を指定
+	public Transform bulletStartPosition = null;		//弾の発射位置を取得
+	
+	private	static readonly float MOVE_Z_FRONT = 5.0f;	//前進の速度
+	private	static readonly	float MOVE_Z_BACK = -2.0f;	//後退の速度
+	
+	private static readonly	float ROTATION_Y_KEY = 360.0f;	//回転の速度(キーボード)
+	private	static readonly	float ROTATION_Y_MOUSE = 720.0f;	//回転の速度(マウス)
+	
+	private	float m_rotationY = 0.0f;		//プレーヤーの回転角度
+	
+	private	bool m_mouseLockFlag = true;		//マウスを固定する機能
+	
 
-
-
-/*
- *	プレーヤークラス
- *	Maruchu
- *
- *	キャラクターの移動、メカニム(モーション)の制御など
- */
-public		class		Player				: MonoBehaviour {
-	
-	
-	
-	public							GameObject	playerObject			= null;		//動かす対象のモデル
-	public							GameObject	bulletObject			= null;		//弾プレハブ
-	
-	public							Transform	bulletStartPosition		= null;		//弾の発射位置を取得するボーン
-	
-	
-	
-	private		static readonly		float		MOVE_Z_FRONT			=  5.0f;	//前進の速度
-	private		static readonly		float		MOVE_Z_BACK				= -2.0f;	//後退の速度
-	
-	private		static readonly		float		ROTATION_Y_KEY			= 360.0f;	//回転の速度(キーボード)
-	private		static readonly		float		ROTATION_Y_MOUSE		= 720.0f;	//回転の速度(マウス)
-	
-	private							float		m_rotationY				= 0.0f;		//プレーヤーの回転角度
-	
-	private							bool		m_mouseLockFlag			= true;		//マウスを固定する機能
-	
-	
-	
-	
-	/*
-	 *	毎フレーム呼び出される関数
-	 */
-	private		void	Update() {
-		
-		//ステージクリアしていたら操作を無視
-		if( Game.IsStageCleared()) {
-			return;
-		}
+	private	void Update() {
 		
 		//マウスロック処理
 		CheckMouseLock();
@@ -52,96 +30,84 @@ public		class		Player				: MonoBehaviour {
 	}
 	
 	
-	/*
-	 *	マウスロック処理のチェック
-	 */
-	private		void	CheckMouseLock() {
+	//マウスロック処理のチェック
+	private	void CheckMouseLock() {
 		
 		//Escキーをおした時の動作
 		if( Input.GetKeyDown( KeyCode.Escape)) {
-			//フラグをひっくり返す
+			//フラグを切り替える
 			m_mouseLockFlag	= !m_mouseLockFlag;
 		}
 		
-		//マウスロックされてる？
+		//マウスロックされているか
 		if( m_mouseLockFlag) {
 			//ロックしていたらロック解除
-			Screen.lockCursor	= true;
-			Cursor.visible	= false;
+			Screen.lockCursor = true;
+			Cursor.visible = false;
 		} else {
 			//ロック解除されていたらロック
-			Screen.lockCursor	= false;
-			Cursor.visible	= true;
+			Screen.lockCursor = false;
+			Cursor.visible = true;
 		}
 	}
-	/*
- *	移動処理のチェック
- */
-	private		void	CheckMove() {
+
+	//移動処理のチェック
+	private	void CheckMove() {
 		
 		//回転
 		{
-			//このフレームで動く回転量
-			float	addRotationY	= 0.0f;
+			//回転量
+			float addRotationY = 0.0f;
 						
 			//キー操作による回転
 			if( Input.GetKey( KeyCode.Q)) {
-				addRotationY		= -ROTATION_Y_KEY;
-			} else
-			if( Input.GetKey( KeyCode.E)) {
-				addRotationY		=  ROTATION_Y_KEY;
+				addRotationY = -ROTATION_Y_KEY;
+			}
+            else if( Input.GetKey( KeyCode.E)) {
+				addRotationY = ROTATION_Y_KEY;
 			}
 			
-			//マウスの移動量による回転
+			//マウスによる回転
 			if( m_mouseLockFlag) {
 				//移動量を取得して角度に渡す
-				addRotationY		+= (Input.GetAxis( "Mouse X")	*ROTATION_Y_MOUSE);
+				addRotationY += (Input.GetAxis("Mouse X") *ROTATION_Y_MOUSE);
 			}
 			
 			//現在の角度に加算する
-			m_rotationY			+= (addRotationY	*Time.deltaTime);		//移動量、回転量には Time.deltaTime を掛けて実行環境(フレーム数の差)による違いが出ないようにする
+			m_rotationY	+= (addRotationY * Time.deltaTime);
 			
 			//オイラー角で入れる
-			transform.rotation	= Quaternion.Euler( 0, m_rotationY, 0);		//Y軸回転でキャラの向きを横に動かせる
+			transform.rotation = Quaternion.Euler( 0, m_rotationY, 0);		//Y軸回転でキャラの向きを横に動かせる
 		}
 		
 		//移動
 		Vector3	addPosition	= Vector3.zero;		//移動量(z の値はメカニムにも渡す)
 		{
-			/*
-				Vector3.zero は new Vector3( 0f, 0f, 0f) と同じです
-				ほかにもいろいろあるので下記のページを参照してほしい
-				http://docs.unity3d.com/ScriptReference/Vector3.html
-			 */
 			
 			//キー操作から移動する量を取得する
-			Vector3	vecInput		= new Vector3( 0f, 0, Input.GetAxisRaw( "Vertical"));		//Zに前後の入力を入れます(Wキー、Sキー、ゲームパッドの入力など)
+			Vector3	vecInput = new Vector3( 0f, 0, Input.GetAxisRaw("Vertical") );		//Zに前後の入力を入れます(Wキー、Sキー、ゲームパッドの入力など)
 			
 			//Z に何か値が入っているか確認する
 			if( vecInput.z > 0) {
 				//前進
-				addPosition.z		= MOVE_Z_FRONT;
+				addPosition.z = MOVE_Z_FRONT;
 			} else
 			if( vecInput.z < 0) {
 				//後退
-				addPosition.z		= MOVE_Z_BACK;
+				addPosition.z = MOVE_Z_BACK;
 			}
 			
 			//移動量をTransformに渡して移動させる
-			transform.position	+= ((transform.rotation	 	*addPosition)		*Time.deltaTime);
-			/*
-				Vector3にtransform.rotationを掛けると、その方向へ曲げてくれる
-				この時、Vector3 は Z+ の方向を正面として考える
-			 */
+			transform.position += ((transform.rotation * addPosition) * Time.deltaTime);
 		}
 		
 		//射撃
-		bool	shootFlag;
+		bool shootFlag;
 		{
 			//射撃ボタン(クリック)押しているか確認する
 			if( Input.GetButtonDown( "Fire1")) {
 				//撃つ処理
-				shootFlag	= true;
+				shootFlag = true;
 				
 				//弾を発車する位置のボーンはあるか確認する
 				if( null!=bulletStartPosition) {
@@ -157,7 +123,7 @@ public		class		Player				: MonoBehaviour {
 				}
 			} else {
 				//撃たない処理
-				shootFlag	= false;
+				shootFlag = false;
 			}
 		}
 		
@@ -165,11 +131,11 @@ public		class		Player				: MonoBehaviour {
 		//メカニム(モーション)
 		{
 			//アニメーターを取得
-			Animator	animator	= playerObject.GetComponent<Animator>();
+			Animator animator = playerObject.GetComponent<Animator>();
 			
 			//Animatorで設定した値を渡す
-			animator.SetFloat(	"SpeedZ",	addPosition.z);	//Z(前後の移動量)
-			animator.SetBool(	"Shoot",	shootFlag);		//射撃フラグ
+			animator.SetFloat( "SpeedZ", addPosition.z);	//Z(前後の移動量)
+			animator.SetBool( "Shoot", shootFlag);		//射撃フラグ
 		}
 	}
 	
